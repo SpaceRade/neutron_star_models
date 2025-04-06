a7240f84f926bd6a6413df778ea3720fcd063d51
kali <kali@kali>
2025-04-06 12:00:00 +0100

    v2: Improved bisection solver and EOS accuracy

--- neutron_star_tov.py
+++ neutron_star_tov.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 """
-TOV Neutron Star Model - Initial Skeleton
+TOV Neutron Star Model - EOS functions added
 """
 
+import numpy as np
+
+# Physical constants
+M_N = 1.675e-27
+C = 2.998e8
+HBAR = 1.055e-34
+EPSILON_0 = (M_N**4 * C**5) / (np.pi**2 * HBAR**3)
+
+def epsilon_x(x):
+    """Energy density as a function of dimensionless momentum x."""
+    return EPSILON_0 / 8 * ((2*x**3 + x)*np.sqrt(1+x**2) - np.arcsinh(x))
+
+def pressure_x(x):
+    """Pressure as a function of dimensionless momentum x."""
+    return EPSILON_0 / 24 * ((2*x**3 - 3*x)*np.sqrt(1+x**2) + 3*np.arcsinh(x))
+
 def run_simulation():
-    # Placeholder for simulation
     pass
 
 def main():
